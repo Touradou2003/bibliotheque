21cf7fc9a699388d25c15a1de395997d40a23ab9
Touradou2003 <cheikhtouradougueye@esp.sn>
2024-04-12 23:40:48 +0000

    Initial commit

---
+++ src/main/java/com/cheikh/bibliotheque2/Bibliotheque2Application.java
@@ -0,0 +1,13 @@
+package com.cheikh.bibliotheque2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bibliotheque2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bibliotheque2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheikh/bibliotheque2/Bibliotheque2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheikh.bibliotheque2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bibliotheque2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
