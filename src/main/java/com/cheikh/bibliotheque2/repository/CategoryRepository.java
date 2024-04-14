1f6d30e35766d22c6dd2536dbe895f7857a5e3ed
Touradou2003 <cheikhtouradougueye@esp.sn>
2024-04-14 22:36:20 +0000

    fin

--- src/main/java/com/cheikh/bibliotheque2/repository/CategoryRepository.java
+++ src/main/java/com/cheikh/bibliotheque2/repository/CategoryRepository.java
@@ -4,6 +4,7 @@ import com.cheikh.bibliotheque2.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
